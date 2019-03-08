132cb9b49618c13a723c70e6dcc9d1b48b7417b9
ambercelene <47156356+ambercelene@users.noreply.github.com>
2019-03-07 17:23:21 -0800

    Added graph to home fragment.

--- app/src/main/java/com/example/moodtrack/HomeFragment.java
+++ app/src/main/java/com/example/moodtrack/HomeFragment.java
@@ -3,9 +3,12 @@ package com.example.moodtrack;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.PieChart;
 
 import java.util.HashMap;
 
@@ -29,7 +32,8 @@ public class HomeFragment extends Fragment {
 //            happyCount++;
 //
 //        }
-
+        PieChart chart = getActivity().findViewById(R.id.chart);
+        Log.d("PieChart", String.valueOf(chart== null) );
         return inflater.inflate(R.layout.fragment_home, null);
     }
 
